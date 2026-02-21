a373a5d924859bfb3275b37eea96f769cdee9ce4
Glebuar <glebuar@gmail.com>
2026-02-21 04:00:46 -0800

    fix CustomPartnerInfo: make required fields optional for API compatibility

--- src/boomi/models/custom_partner_info.py
+++ src/boomi/models/custom_partner_info.py
@@ -27,24 +27,26 @@ class CustomPartnerInfo(BaseModel):
 
     def __init__(
         self,
-        custom_control_info: CustomControlInfo,
-        custom_options: CustomOptions,
+        custom_control_info: CustomControlInfo = SENTINEL,
+        custom_options: CustomOptions = SENTINEL,
         custom_standard_id: str = SENTINEL,
         **kwargs,
     ):
         """CustomPartnerInfo
 
-        :param custom_control_info: custom_control_info
-        :type custom_control_info: CustomControlInfo
-        :param custom_options: custom_options
-        :type custom_options: CustomOptions
+        :param custom_control_info: custom_control_info, defaults to None
+        :type custom_control_info: CustomControlInfo, optional
+        :param custom_options: custom_options, defaults to None
+        :type custom_options: CustomOptions, optional
         :param custom_standard_id: ID of the Custom Document Standard., defaults to None
         :type custom_standard_id: str, optional
         """
-        self.custom_control_info = self._define_object(
-            custom_control_info, CustomControlInfo
-        )
-        self.custom_options = self._define_object(custom_options, CustomOptions)
+        if custom_control_info is not SENTINEL:
+            self.custom_control_info = self._define_object(
+                custom_control_info, CustomControlInfo
+            )
+        if custom_options is not SENTINEL:
+            self.custom_options = self._define_object(custom_options, CustomOptions)
         if custom_standard_id is not SENTINEL:
             self.custom_standard_id = custom_standard_id
         self._kwargs = kwargs
